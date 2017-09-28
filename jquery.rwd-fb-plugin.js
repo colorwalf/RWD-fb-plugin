ea0402aab38183440e0242c1bf1a7462e9059d32
Frank <kuofp@cs.nctu.edu.tw>
2017-09-28 13:02:38 +0800

    [update] another proper way

--- jquery.rwd-fb-plugin.js
+++ jquery.rwd-fb-plugin.js
@@ -1,26 +1,21 @@
 jQuery.fn.extend({
 	rwd: function(){
+		$(this).wrap('<div style="width: 100%; overflow: hidden;"></div>');
+		
 		var fb = this;
-		var id = 'fb-box-container-' + Math.floor(Math.random() * 10000);
-		$(fb).wrap('<div id="' + id + '" style="width: 100%; height: 100%;">');
+		var tm = $(fb).parent();
 		
 		$(window).resize(function(){
-			var r_w = 1;
-			//clear transform
-			$(fb).attr('style', '');
-			
-			var win_w = $('#' + id).width();
 			
 			//default width height 500px
 			var fb_w = parseInt($(fb).attr('data-width')) || 500;
-			var fb_h = fb_w;
-			r_w = win_w/fb_w;
+			var fb_h = parseInt($(fb).attr('data-height'))|| 500;
+			var win_w = tm.width();
+			var r_w = win_w/fb_w;
 			
-			$(fb)
-				.css('left', ((fb_w*r_w-fb_w)/2) + 'px')
-				.css('top',  ((fb_h*r_w-fb_h)/2) + 'px')
-				.css('transform', 'scale('+r_w+', '+r_w+')');
-				
+			$(fb).css('transform', 'scale('+r_w+')').css('transform-origin', '0 0');
+			$(tm).css('height', fb_h*r_w + 'px');
+			
 		}).resize();
 	}
 });
